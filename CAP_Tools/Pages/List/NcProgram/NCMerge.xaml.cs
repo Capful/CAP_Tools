82c2de6e41958f6fe780465bbf0bfb345d8d09fc
Capful <Capful@outlook.com>
2018-08-07 20:38:18 +0800

    1

---
+++ CAP_Tools/Pages/List/NcProgram/NCMerge.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CAP_Tools.Pages.List.NcProgram
+{
+    /// <summary>
+    /// Interaction logic for NCMerge.xaml
+    /// </summary>
+    public partial class NCMerge : UserControl
+    {
+        public NCMerge()
+        {
+            InitializeComponent();
+        }
+
+        private void CL_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LL_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

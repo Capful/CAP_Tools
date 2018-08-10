1ffbe5e4f0c075b8f5de4032d09a6921a9771cdf
袁盛华 <capful@outlook.com>
2018-08-10 20:37:42 +0800

    删除不必要的Using

--- CAP_Tools/Pages/List/NcProgram/NCWcs.xaml.cs
+++ CAP_Tools/Pages/List/NcProgram/NCWcs.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using FirstFloor.ModernUI.Windows.Controls;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CAP_Tools.Pages.List.NcProgram
 {

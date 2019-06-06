3495e7c0ab606f5c0849e9dcea2b172b9ad22f7c
HZK-PC <Capful@outlook.com>
2019-06-06 22:58:52 +0800

    ini配置文件测试

---
+++ CAP_Tools/Pages/List/NcProgram/NcConfig.xaml.cs
@@ -0,0 +1,45 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CAP_Tools.Pages.List
+{
+    /// <summary>
+    /// Interaction logic for NcTabPage.xaml
+    /// </summary>
+    public partial class NcConfig : UserControl
+    {
+        public NcConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IniFile iniFile = new IniFile(".///NC Config//星创后处理.ini");
+            //iniFile.writeIni("section1", "key1", "value11");
+
+
+            WCS_Line.Text = iniFile.ReadIni("WCS_Config", "WCS_Line");
+            WCS_Start.Text = iniFile.ReadIni("WCS_Config", "WCS_Start");
+            WCS_End.Text = iniFile.ReadIni("WCS_Config", "WCS_End");
+
+            T_Line.Text = iniFile.ReadIni("T_Config", "T_Line");
+            T_Start.Text = iniFile.ReadIni("T_Config", "T_Start");
+            T_End.Text = iniFile.ReadIni("T_Config", "T_End");
+
+
+
+           
+        }
+
+        private void PZ_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string a = PZ.Text;
+            ModernDialog.ShowMessage(a, "警告", MessageBoxButton.OK);
+        }
+    }
+}
--- CAP_Tools/Properties/AssemblyInfo.cs
+++ CAP_Tools/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CAP Tools")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -38,7 +38,7 @@ using System.Windows.Media;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.1906")]
+[assembly: AssemblyFileVersion("1.3.1906")]
 [assembly: NeutralResourcesLanguage("zh-Hans")]
 
